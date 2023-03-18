774e3f82c6456944ce7abc9df530e86bb08bc923
Tony Messias <tonysm@hey.com>
2023-03-18 19:26:34 -0300

    MRSK Demo

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '167.172.14.76';
 
     /**
      * The headers that should be used to detect proxies.
